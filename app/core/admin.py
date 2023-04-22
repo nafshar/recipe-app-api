bca656143221f2e047c62a5dd2630c69ffe20f71
Nader Afshar <nafshar@persisconsulting.com>
2023-04-22 15:45:52 -0400

    added admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin  # noqa
+"""
+Django Admin Customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+# The following import will show an error in IDE
+# However we need to ignore this error as we run this
+# odule inside a Docker Container which uses a different
+# path settings to get to the "core" module and "models" file.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """ Define the admin pages for users """
+    ordering = ['id']
+    list_display = ['email', 'name']
+
+
+admin.site.register(models.User, UserAdmin)
