f19a7a0e8f95f042bf5b2db6be50436966f1e71f
Nader Afshar <nafshar@persisconsulting.com>
2023-04-22 16:16:53 -0400

    updated admin.py

--- app/core/admin.py
+++ app/core/admin.py
@@ -3,6 +3,7 @@ Django Admin Customization
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 # The following import will show an error in IDE
 # However we need to ignore this error as we run this
@@ -15,6 +16,21 @@ class UserAdmin(BaseUserAdmin):
     """ Define the admin pages for users """
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields' : ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 
 admin.site.register(models.User, UserAdmin)
