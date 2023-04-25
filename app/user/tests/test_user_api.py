ad17696aec7cf413f80fac237c86e0ad680b7c12
Nader Afshar <nafshar@persisconsulting.com>
2023-04-25 09:46:11 -0400

    fixed errors in user_test_api

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -98,5 +98,5 @@ class PublicUserApiTests(TestCase):
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
-        self.assertIn('token', res.data)
+        self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
